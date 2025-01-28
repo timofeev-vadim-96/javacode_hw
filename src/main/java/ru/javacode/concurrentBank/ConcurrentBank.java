69e28124c9fdb01c92d55d0cf6b6988f7df80cd0
Vadim Timofeev <timofeev.vadim.96@mail.ru>
2025-01-28 17:15:07 +0300

    fix mentor#1

--- src/main/java/ru/javacode/concurrentBank/ConcurrentBank.java
+++ src/main/java/ru/javacode/concurrentBank/ConcurrentBank.java
@@ -3,12 +3,16 @@ package ru.javacode.concurrentBank;
 import ru.javacode.concurrentBank.exception.InsufficientFundsException;
 
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.Semaphore;
 
 public class ConcurrentBank implements Bank {
     private final CopyOnWriteArrayList<BankAccount> accounts;
 
+    private final Semaphore semaphore;
+
     public ConcurrentBank() {
         this.accounts = new CopyOnWriteArrayList<>();
+        semaphore = new Semaphore(1);
     }
 
     @Override
@@ -21,18 +25,29 @@ public class ConcurrentBank implements Bank {
 
     @Override
     public void transfer(BankAccount sender, BankAccount recipient, long amount) {
-        long senderBalance = sender.getAccount();
-        long recipientBalance = recipient.getAccount();
-        if (sender.getAccount() >= amount) { //если достаточно средств
-            long senderNewBalance = sender.withdraw(amount);
-            if (senderNewBalance == senderBalance - amount) { //если деньги списались
-                long recipientNewBalance = recipient.deposit(amount);
-                if (recipientNewBalance != recipientBalance + amount) { //если деньги не зачислились
-                    sender.deposit(amount);
+        try {
+            semaphore.acquire();
+            synchronized (sender) {
+                synchronized (recipient) {
+                    long senderBalance = sender.getAccount();
+                    long recipientBalance = recipient.getAccount();
+                    if (sender.getAccount() >= amount) { //если достаточно средств
+                        long senderNewBalance = sender.withdraw(amount);
+                        if (senderNewBalance == senderBalance - amount) { //если деньги списались
+                            long recipientNewBalance = recipient.deposit(amount);
+                            if (recipientNewBalance != recipientBalance + amount) { //если деньги не зачислились
+                                sender.deposit(amount);
+                            }
+                        }
+                    } else {
+                        throw new InsufficientFundsException("Недостаточно средств");
+                    }
                 }
             }
-        } else {
-            throw new InsufficientFundsException("Недостаточно средств");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            semaphore.release();
         }
     }
 
